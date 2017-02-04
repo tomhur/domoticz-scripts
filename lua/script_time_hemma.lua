6665685ea93b0779c44bf804287f0de8e9490c1a
Tommy Hurtig <tommy@hurtig.xyz>
2017-02-04 22:12:03 +0100

    Turn on lights if it is dark enough
    Checks how bright it is in the house and turns on lights if
    someone is home.

--- lua/script_time_hemma.lua
+++ lua/script_time_hemma.lua
@@ -1,18 +1,30 @@
 dofile("/opt/domoticz/scripts/parse_config.lua")
+debug = false
 
 commandArray = {}
 
 time = os.date("*t")
 minutes = time.min + time.hour * 60
 
-if (minutes > (timeofday['SunsetInMinutes']) and otherdevices[conf['atHome']] == 'On' and otherdevices[conf['eveningLights']] == 'Off'  and otherdevices[conf['atSleep']] == 'Off' ) then
+if debug then print('light=' .. otherdevices[conf['lightIntensity']]) end
+if (tonumber(otherdevices[conf['lightIntensity']]) < tonumber(conf['switchOnLux'])) then
+	switchOnLux = true
+else
+	switchOnLux = false
+end
+
+if ((minutes > (timeofday['SunsetInMinutes']) or switchOnLux) and otherdevices[conf['atHome']] == 'On' and otherdevices[conf['eveningLights']] == 'Off'  and otherdevices[conf['atSleep']] == 'Off' ) then
   commandArray[conf['eveningLightsScene']]='On'
 end
-if (minutes < (timeofday['SunriseInMinutes']) and otherdevices[conf['atHome']] == 'On' and otherdevices[conf['atSleep']] == 'Off' and otherdevices[conf['eveningLights']] == 'Off' ) then
+if ((minutes < (timeofday['SunriseInMinutes']) or switchOnLux ) and otherdevices[conf['atHome']] == 'On' and otherdevices[conf['atSleep']] == 'Off' and otherdevices[conf['eveningLights']] == 'Off' ) then
   commandArray[conf['eveningLightsScene']]='On'
 end
 if (minutes > (timeofday['SunriseInMinutes']) and minutes < (timeofday['SunsetInMinutes']) and otherdevices[conf['eveningLights']] == 'On' ) then
-  commandArray[conf['eveningLightsScene']]='Off'
+  if ( otherdevices[conf['atHome']] == 'On' and switchOnLux ) then
+    if debug then print('It is still dark. Do not turn off lights') end
+  else
+    commandArray[conf['eveningLightsScene']]='Off'
+  end
 end
 
 if (otherdevices[conf['atHome']] == 'Off' ) then
