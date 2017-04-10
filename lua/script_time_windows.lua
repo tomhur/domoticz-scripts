84b1aa3cb23e14953c6eb8e5607e1a45e36d8426
Tommy Hurtig <tommy@hurtig.xyz>
2017-04-10 19:44:55 +0200

    Do not trigger window lights by illuminance

--- lua/script_time_windows.lua
+++ lua/script_time_windows.lua
@@ -10,14 +10,6 @@ minutes = time.min + time.hour * 60
 morning = tonumber(conf['lightsAtMorning'])*60
 night = tonumber(conf['lightsAtNight'])*60
 
-if debug then print('light=' .. otherdevices[conf['lightIntensity']]) end
-if (tonumber(otherdevices[conf['lightIntensity']]) < tonumber(conf['switchOnLux'])) then 
-    switchOnLux = true
-else
-    switchOnLux = false
-end
-
-
 --turn on at morning
 if (minutes < (timeofday['SunriseInMinutes']) and minutes > morning and otherdevices[conf['windowLights']] == 'Off' ) then
   if debug then print('Turn on lights at morning') end
@@ -26,17 +18,12 @@ end
 
 --turn off during day
 if ( otherdevices[conf['windowLights']] == 'On' and minutes > (timeofday['SunriseInMinutes']) and minutes < (timeofday['SunsetInMinutes']) ) then
-  if ( otherdevices[conf['atHome']] == 'On' and switchOnLux ) then
-    if debug then print('It is still dark. Keep lights on') end
-  else
-  if debug then print('Turn of lights during day') end
-    commandArray[conf['windowLightsScene']]='Off'
-    commandArray[conf['windowLightsEveningScene']]='Off'
-  end
+  commandArray[conf['windowLightsScene']]='Off'
+  commandArray[conf['windowLightsEveningScene']]='Off'
 end
 
 --turn on at evening
-if ( (minutes > (timeofday['SunsetInMinutes']) or switchOnLux ) and otherdevices[conf['windowLights']] == 'Off' and otherdevices[conf['atSleep']] == 'Off' ) then
+if ( minutes > (timeofday['SunsetInMinutes']) and otherdevices[conf['windowLights']] == 'Off' and otherdevices[conf['atSleep']] == 'Off' ) then
   if debug then print('Turn on lights at evening') end
   commandArray[conf['windowLightsScene']]='On'
   commandArray[conf['windowLightsEveningScene']]='On'
